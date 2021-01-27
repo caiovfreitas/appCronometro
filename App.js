34d903392e24626a40172953e5ac5108cb9ab7b2
Caio Freitas <cvaf@cin.ufpe.br>
2021-01-27 13:15:09 -0300

    Primeiro Commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+class App extends Component {
+
+  render(){
+    return(
+      <View>
+        <Text>Cronometro</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
